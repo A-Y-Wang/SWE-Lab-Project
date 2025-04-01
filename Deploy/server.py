831fcc954443aea44489f05d2c153f12be45f25d
sarahmobley343 <sarah.mobley@gmail.com>
2025-04-01 16:37:09 -0500

    initial commit for heroku

---
+++ Deploy/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os
+ 
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+
+def create_app():
+    from backend.usercontrol import users_bp
+    app.register_blueprint(users_bp, url_prefix='/users')
+    return app
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
